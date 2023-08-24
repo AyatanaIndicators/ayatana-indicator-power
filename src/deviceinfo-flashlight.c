1ccd548f592f0cf3e65cb33c6b2142429918042d
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2023-08-24 16:43:35 +0200

    src/deviceinfo-flashlight.c: Add copyright header.
    Fixes https://github.com/AyatanaIndicators/ayatana-indicator-power/issues/82

--- src/deviceinfo-flashlight.c
+++ src/deviceinfo-flashlight.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2023 The UBports project
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Muhammad <muhammad23012009@hotmail.com>
+ */
+
 #include <deviceinfo_c_api.h>
 
 char* flashlight_path()
