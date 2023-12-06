18d92e36d1d08affd20ae905f6a9efdb139af170
Ratchanan Srirattanamet <ratchanan@ubports.com>
2023-12-06 23:52:09 +0700

    Revert "Remove orphan osd-notify remnants"
    As discussed in [1], these hints are essential to make the indicator
    functions correctly on Lomiri. These hints should not make any
    difference on DE's that doesn't support them.
    
    [1] https://gitlab.com/ubports/development/core/content-hub/-/merge_requests/32#note_1552217874
    
    This reverts commit ba37bd3998252a759434a8455e52f00699b50a09.

--- tests/test-notify.cc
+++ tests/test-notify.cc
@@ -72,6 +72,8 @@ protected:
   static constexpr char const * METHOD_GET_INFO {"GetServerInformation"};
   static constexpr char const * SIGNAL_CLOSED {"NotificationClosed"};
 
+  static constexpr char const * HINT_TIMEOUT {"x-lomiri-snap-decisions-timeout"};
+
 protected:
 
   void SetUp()
